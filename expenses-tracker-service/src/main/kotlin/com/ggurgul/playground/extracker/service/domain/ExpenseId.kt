f07cf9b50fcd289951b7018b9a38ce224a92eea7
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-10-28 16:23:46 +0200

    Returning expenses

--- expenses-tracker-service/src/main/kotlin/com/ggurgul/playground/extracker/service/domain/ExpenseId.kt
+++ expenses-tracker-service/src/main/kotlin/com/ggurgul/playground/extracker/service/domain/ExpenseId.kt
@@ -27,4 +27,23 @@ class ExpenseId(
 
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ExpenseId
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun toString(): String {
+        return id.toString()
+    }
+
 }
