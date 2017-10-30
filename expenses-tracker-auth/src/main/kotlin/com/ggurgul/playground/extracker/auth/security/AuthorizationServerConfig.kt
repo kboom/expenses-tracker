c5207d05628eaf83b0c293eef5f4c9db98c614af
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-10-30 21:25:12 +0100

    Moving resource-related security config to the resource itself

--- expenses-tracker-auth/src/main/kotlin/com/ggurgul/playground/extracker/auth/security/AuthorizationServerConfig.kt
+++ expenses-tracker-auth/src/main/kotlin/com/ggurgul/playground/extracker/auth/security/AuthorizationServerConfig.kt
@@ -1,5 +1,6 @@
 package com.ggurgul.playground.extracker.auth.security
 
+import com.ggurgul.playground.extracker.auth.services.SecurityUserAdapter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -15,15 +16,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
 import org.springframework.security.oauth2.provider.OAuth2Authentication
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices
-import org.springframework.security.oauth2.provider.token.TokenEnhancer
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory
 
-import java.util.HashMap
-
 import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.provider.token.*
+import java.util.*
+
 
 @Configuration
 @EnableAuthorizationServer
@@ -31,6 +33,9 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
 
     @Autowired
     private lateinit var authenticationManager: AuthenticationManager
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
 
     @Value("\${keystore.password}")
     private val pwd: String? = null
@@ -57,7 +62,9 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
     override fun configure(endpoints: AuthorizationServerEndpointsConfigurer?) {
         endpoints!!
                 .tokenStore(tokenStore())
-                .tokenEnhancer(jwtTokenConverter())
+                .tokenEnhancer(TokenEnhancerChain().apply {
+                    setTokenEnhancers(Arrays.asList(CustomTokenEnhancer(), jwtTokenConverter()))
+                })
                 .authenticationManager(authenticationManager)
     }
 
@@ -84,18 +91,29 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
         )
         val converter = JwtAccessTokenConverter()
         converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"))
-        return converter
+        converter.accessTokenConverter = DefaultAccessTokenConverter().apply {
+            setUserTokenConverter(
+                    // todo use extended DefaultUserAuthenticationConverter (use super + extract user details from the provided JWT map
+                    // todo no need to contact database, all details already there (especially important if it was being done in remote application)
+                    DefaultUserAuthenticationConverter().apply {
+                        setUserDetailsService(userDetailsService)
+                        // todo this will make principal of SecurityContext SecurityUserAdapter
+                    }
+            )
+        }
+        return converter;
     }
 
-//    class CustomTokenEnhancer : TokenEnhancer {
-//
-//        override fun enhance(accessToken: OAuth2AccessToken, authentication: OAuth2Authentication): OAuth2AccessToken {
-//            val additionalInfo = HashMap<String, Any>()
-//            additionalInfo.put("organization", authentication.name + randomAlphabetic(4))
-//            (accessToken as DefaultOAuth2AccessToken).additionalInformation = additionalInfo
-//            return accessToken
-//        }
-//
-//    }
+    class CustomTokenEnhancer : TokenEnhancer {
+
+        override fun enhance(accessToken: OAuth2AccessToken, authentication: OAuth2Authentication): OAuth2AccessToken {
+            val additionalInfo = HashMap<String, Any>()
+            val authenticatedUser = (authentication.principal as SecurityUserAdapter).user
+            additionalInfo.put("email", authenticatedUser.email)
+            (accessToken as DefaultOAuth2AccessToken).additionalInformation = additionalInfo
+            return accessToken
+        }
+
+    }
 
 }
